6c56e3c88786a01159e0568ac94a80f071749ab3
Yashraj Jadhav <yashrajjadhav49@gmail.com>
2023-03-28 23:18:33 +0530

    added authenticate method

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -4,50 +4,70 @@
 //
 //  Created by Yashraj jadhav on 26/03/23.
 //
-
+import MapKit
 import SwiftUI
+import LocalAuthentication
 
 struct ContentView: View {
-    let users = [
-        User(firstName: "Arnold", LastName: "Rimmer"),
-        User(firstName: "Kristine", LastName: "Kochanski"),
-        User(firstName: "David", LastName: "Lister"),
-    ].sorted()
     
-
+    
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    
+    //MARK: - array of the desird locations
+    
+    let locations = [Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)) ,
+                     
+                     Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+                     
+    ]
+    
     var body: some View {
-        Text("Hello World")
-            .onTapGesture {
-                let str = "Test Message"
-                let url = getDocumentsDirectory().appendingPathComponent("message.txt")
-                
-                do {
-                    try str.write(to: url , atomically: true , encoding: .utf8)
-                    let input = try String(contentsOf: url)
-                    print(input)
-                } catch {
-                    print(error.localizedDescription)
+        NavigationView {
+            Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+                MapAnnotation(coordinate: location.coordinate) {
+                    NavigationLink {
+                        Text(location.name)
+                    } label: {
+                        Circle()
+                            .stroke(.red, lineWidth: 3)
+                            .frame(width: 44, height: 44)
+                    }
                 }
             }
+            .navigationTitle("London Explorer")
+        }
     }
-    func getDocumentsDirectory() -> URL {
-        // find all possible documents directories for this user
-        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    
+    func authenticate() {
         
-        // just send back the first one, which ought to be the only one
-
-        return path[0]
+        let context = LAContext()
+        var error: NSError?
+        
+        // check whether biometric authentication is possible
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            // it's possible, so go ahead and use it
+            let reason = "We need to unlock your data."
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                // authentication has now completed
+                if success {
+                    // authenticated successfully
+                } else {
+                    // there was a problem
+                }
+            }
+        } else {
+            // no biometrics
+        }   
     }
 }
 
-struct User : Identifiable ,Comparable {
+
+struct Location : Identifiable {
     let id = UUID()
-    let firstName : String
-    let LastName : String
+    let name : String
+    let coordinate : CLLocationCoordinate2D
     
-    static func <(lhs:User , rhs:User) ->Bool {
-        lhs.LastName < rhs.LastName
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
