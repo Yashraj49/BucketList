e47d27026384e8e948edc6892baa0bd7c785a4f4
Yashraj Jadhav <yashrajjadhav49@gmail.com>
2023-03-29 22:58:26 +0530

    Update ContentView.swift

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -1,74 +1,108 @@
-//
-//  ContentView.swift
-//  BucketList
-//
-//  Created by Yashraj jadhav on 26/03/23.
-//
 import MapKit
 import SwiftUI
 import LocalAuthentication
 
 struct ContentView: View {
+    @State private var selectedPlace: Location?
+    @State private var locations = [Location]()
+    @State private var isUnlocked = false
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
     
     
-    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
     
-    //MARK: - array of the desird locations
-    
-    let locations = [Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)) ,
-                     
-                     Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
-                     
-    ]
     
     var body: some View {
-        NavigationView {
+        
+        ZStack{
             Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
                 MapAnnotation(coordinate: location.coordinate) {
-                    NavigationLink {
-                        Text(location.name)
-                    } label: {
-                        Circle()
-                            .stroke(.red, lineWidth: 3)
+                    VStack {
+                        Image(systemName: "star.circle")
+                            .resizable()
+                            .foregroundColor(.red)
                             .frame(width: 44, height: 44)
+                            .background(.white)
+                            .clipShape(Circle())
+                        
+                        Text(location.name)
+                            .fixedSize()
                     }
-                }
+                    .onTapGesture {
+                      selectedPlace = location
+                    }
+                 }
             }
-            .navigationTitle("London Explorer")
-        }
-    }
-    
-    func authenticate() {
-        
-        let context = LAContext()
-        var error: NSError?
-        
-        // check whether biometric authentication is possible
-        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
-            // it's possible, so go ahead and use it
-            let reason = "We need to unlock your data."
+            .ignoresSafeArea()
+            Circle()
+                .fill(.blue)
+                .opacity(0.3)
+                .frame(width: 32 , height: 32)
+            VStack{
+                Spacer()
+                HStack{
+                    Spacer()
+                    Button{
+                        var newLocation = Location(id: UUID(), name: "New Location", description: "", latitude: mapRegion.center.latitude, longitude: mapRegion.center.longitude)
+                        locations.append(newLocation)
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                    .padding()
+                    .background(.black.opacity(0.75))
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .clipShape(Circle())
+                    .padding(.trailing)
+                    
+                }
+                
+            }
             
-            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
-                // authentication has now completed
-                if success {
-                    // authenticated successfully
-                } else {
-                    // there was a problem
+        }
+        .sheet(item: $selectedPlace) {place in
+            Text(place.name)
+            
+             //MARK: - accepts the new location, then looks up where the current location is and replaces it in the array. This will cause our map to update immediately with the new data.
+            
+            EditView(location: place) { newLocation in
+                if let index = locations.firstIndex(of: place) {
+                    locations[index] = newLocation
                 }
             }
-        } else {
-            // no biometrics
-        }   
+        }
+        
     }
-}
-
-
-struct Location : Identifiable {
-    let id = UUID()
-    let name : String
-    let coordinate : CLLocationCoordinate2D
     
 }
+        
+//        func authenticate() {
+//
+//            let context = LAContext()
+//            var error: NSError?
+//
+//            // check whether biometric authentication is possible
+//            if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+//                // it's possible, so go ahead and use it
+//                let reason = "We need to unlock your data."
+//
+//                context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+//
+//                    // authentication has now completed
+//
+//                    if success {
+//                        isUnlocked = true
+//                    } else {
+//                        // there was a problem
+//                    }
+//                }
+//            } else {
+//                // no biometrics
+//            }
+//        }
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
