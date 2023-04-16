ac2798e3c3a6e5409818be4c3e50146c1fc418bf
Yashraj Jadhav <yashrajjadhav49@gmail.com>
2023-04-16 21:52:24 +0530

    Update EditView.swift

--- BucketList/EditView.swift
+++ BucketList/EditView.swift
@@ -4,60 +4,58 @@
 //
 //  Created by Yashraj jadhav on 29/03/23.
 //
-
-//
-//  EditView.swift
-//  BucketList
-//
-//  Created by Yashraj jadhav on 29/03/23.
-//
-
 import SwiftUI
 
 struct EditView: View {
+    enum LoadingState {
+        case loading, loaded, failed
+    }
+
     @Environment(\.dismiss) var dismiss
-    var location : Location
-    var onSave : (Location) -> Void
-    @State private var name : String
-    @State private var description : String
-    
+    var location: Location
+    var onSave: (Location) -> Void
+
+    @State private var name: String
+    @State private var description: String
+
     @State private var loadingState = LoadingState.loading
     @State private var pages = [Page]()
-    
+
     var body: some View {
-        NavigationView{
+        NavigationView {
             Form {
                 Section {
-                    TextField("Place name" , text: $name)
-                    TextField("Description" , text: $description)
+                    TextField("Place name", text: $name)
+                    TextField("Description", text: $description)
                 }
+
                 Section("Nearby…") {
                     switch loadingState {
+                    case .loading:
+                        Text("Loading…")
                     case .loaded:
                         ForEach(pages, id: \.pageid) { page in
                             Text(page.title)
                                 .font(.headline)
-                            + Text(": ") +
-                            Text("Page description here")
+                            + Text(": ")
+                            + Text(page.description)
                                 .italic()
                         }
-                    case .loading:
-                        Text("Loading…")
                     case .failed:
                         Text("Please try again later.")
                     }
                 }
             }
-            .navigationTitle("place details")
-            .toolbar{
-                Button("Save"){
+            .navigationTitle("Place details")
+            .toolbar {
+                Button("Save") {
                     var newLocation = location
                     newLocation.id = UUID()
                     newLocation.name = name
                     newLocation.description = description
-                    
+
                     onSave(newLocation)
-                       dismiss()
+                    dismiss()
                 }
             }
             .task {
@@ -65,7 +63,7 @@ struct EditView: View {
             }
         }
     }
-    
+
     init(location: Location, onSave: @escaping (Location) -> Void) {
         self.location = location
         self.onSave = onSave
@@ -73,6 +71,7 @@ struct EditView: View {
         _name = State(initialValue: location.name)
         _description = State(initialValue: location.description)
     }
+
     func fetchNearbyPlaces() async {
         let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.coordinate.latitude)%7C\(location.coordinate.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
 
@@ -83,41 +82,19 @@ struct EditView: View {
 
         do {
             let (data, _) = try await URLSession.shared.data(from: url)
-
-            // we got some data back!
             let items = try JSONDecoder().decode(Result.self, from: data)
-
-            // success – convert the array values to our pages array
-            pages = items.query.pages.values.sorted { $0.title < $1.title }
+            pages = items.query.pages.values.sorted()
             loadingState = .loaded
         } catch {
-            // if we're still here it means the request failed somehow
             loadingState = .failed
         }
     }
-   
-}
-
-enum LoadingState {
-    case loading , loaded , failed
-}
-
-struct Page: Codable {
-    let pageid: Int
-    let title:  String
-    let pages:  [String: [String]]?
-}
-
-struct Query : Codable {
-    let pages : [Int:Page]
-}
-
-struct Result: Codable {
-    let query: Query
 }
 
 struct EditView_Previews: PreviewProvider {
     static var previews: some View {
-        EditView(location: Location.example) { newLocation in }
+        EditView(location: Location.example) { _ in }
     }
+}
+
 }
