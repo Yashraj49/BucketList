65cf5a10dbc4a650b7af4e7bcd16117cf78a0a85
Yashraj Jadhav <yashrajjadhav49@gmail.com>
2023-04-18 20:31:55 +0530

    Update ContentView.swift

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -6,7 +6,11 @@ import LocalAuthentication
 
 struct ContentView: View {
     @StateObject private var viewModel = ViewModel()
-
+    @State private var showingAuthenticationAlert = false
+    @State private var authenticationError = ""
+    @State private var isUnlocked = false
+    
+    
     var body: some View {
         if viewModel.isUnlocked {
             ZStack {
@@ -19,7 +23,7 @@ struct ContentView: View {
                                 .frame(width: 44, height: 44)
                                 .background(.white)
                                 .clipShape(Circle())
-
+                            
                             Text(location.name)
                                 .fixedSize()
                         }
@@ -29,29 +33,30 @@ struct ContentView: View {
                     }
                 }
                 .ignoresSafeArea()
-
+                
                 Circle()
                     .fill(.blue)
                     .opacity(0.3)
                     .frame(width: 32, height: 32)
-
+                
                 VStack {
                     Spacer()
-
+                    
                     HStack {
                         Spacer()
-
+                        
                         Button {
                             viewModel.addLocation()
                         } label: {
                             Image(systemName: "plus")
+                                .padding()
+                                .background(.black.opacity(0.75))
+                                .foregroundColor(.white)
+                                .font(.title)
+                                .clipShape(Circle())
+                                .padding(.trailing)
                         }
-                        .padding()
-                        .background(.black.opacity(0.75))
-                        .foregroundColor(.white)
-                        .font(.title)
-                        .clipShape(Circle())
-                        .padding(.trailing)
+                        
                     }
                 }
             }
@@ -59,7 +64,13 @@ struct ContentView: View {
                 EditView(location: place) { newLocation in
                     viewModel.update(location: newLocation)
                 }
+            }.alert("Biometric auth Failed",isPresented: $viewModel.ShowingNotAlert) {
+                Button("ok") {}
+            }message: {
+                Text("Please try again")
             }
+            
+            
         } else {
             Button("Unlock Places") {
                 viewModel.authenticate()
@@ -70,41 +81,39 @@ struct ContentView: View {
             .clipShape(Capsule())
         }
     }
+    
+    func authenticate() {
+        
+        let context = LAContext()
+        var error: NSError?
+        
+        // check whether biometric authentication is possible
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            // it's possible, so go ahead and use it
+            let reason = "We need to unlock your data."
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                
+                // authentication has now completed
+                
+                if success {
+                    isUnlocked = true
+                } else {
+                    // there was a problem
+                }
+            }
+        } else {
+            // no biometrics
+        }
+    }
 }
-
-
-
-
-//        func authenticate() {
-//
-//            let context = LAContext()
-//            var error: NSError?
-//
-//            // check whether biometric authentication is possible
-//            if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
-//                // it's possible, so go ahead and use it
-//                let reason = "We need to unlock your data."
-//
-//                context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
-//
-//                    // authentication has now completed
-//
-//                    if success {
-//                        isUnlocked = true
-//                    } else {
-//                        // there was a problem
-//                    }
-//                }
-//            } else {
-//                // no biometrics
-//            }
-//        }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
 
 
 
