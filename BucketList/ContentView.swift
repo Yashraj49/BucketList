b4a5a129ace96be6f55ddd0c9adac7730eb235bd
Yashraj Jadhav <yashrajjadhav49@gmail.com>
2023-04-19 19:01:32 +0530

    Update ContentView.swift

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -1,96 +1,102 @@
+//
+//  ContentView-ViewModel.swift
+//  BucketList
+//
+//  Created by Yashraj jadhav on 16/04/23.
+
+
+// it’s the view model for ContentView.
+
+/// We’re going to use this to create a new class that manages our data, and manipulates it on behalf of the ContentView struct so that our view doesn’t really care how the underlying data system works.
+import UIKit
+import Foundation
 import MapKit
-import SwiftUI
 import LocalAuthentication
+import SwiftUI
+///The main actor is responsible for running all user interface updates, and adding that attribute to the class means we want all its code – any time it runs anything, unless we specifically ask otherwise – to run on that main actor. This is important because it’s responsible for making UI updates, and those must happen on the main actor.
 
+extension ContentView {
+    @MainActor class ViewModel : ObservableObject {
+        let savePath = FileManager.documentsDirectory.appendingPathComponent("SavedPlaces")
+        
+        
+        
+        @Published  var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
+        @Published  private(set) var locations : [Location] = []
+        @Published var selectedPlace: Location?
+     //   @Published var errorTitle : String
+        @Published var isUnlocked = false
+        @Published var ShowingNotAlert = false
+     //   @Published var errorMessage : String
+      //  @Published var hasError = false
+        @Published var errorTitle: String = ""
+        @Published var errorMessage: String = ""
+        @Published var hasError: Bool = false
 
-
-struct ContentView: View {
-    @StateObject private var viewModel = ViewModel()
-    @State private var showingAuthenticationAlert = false
-    @State private var authenticationError = ""
-    @State private var isUnlocked = false
-    
-    
-    var body: some View {
-        if viewModel.isUnlocked {
-            ZStack {
-                Map(coordinateRegion: $viewModel.mapRegion, annotationItems: viewModel.locations) { location in
-                    MapAnnotation(coordinate: location.coordinate) {
-                        VStack {
-                            Image(systemName: "star.circle")
-                                .resizable()
-                                .foregroundColor(.red)
-                                .frame(width: 44, height: 44)
-                                .background(.white)
-                                .clipShape(Circle())
-                            
-                            Text(location.name)
-                                .fixedSize()
-                        }
-                        .onTapGesture {
-                            viewModel.selectedPlace = location
-                        }
-                    }
-                }
-                .ignoresSafeArea()
-                
-                Circle()
-                    .fill(.blue)
-                    .opacity(0.3)
-                    .frame(width: 32, height: 32)
-                
-                VStack {
-                    Spacer()
-                    
-                    HStack {
-                        Spacer()
-                        
-                        Button {
-                            viewModel.addLocation()
-                        } label: {
-                            Image(systemName: "plus")
-                                .padding()
-                                .background(.black.opacity(0.75))
-                                .foregroundColor(.white)
-                                .font(.title)
-                                .clipShape(Circle())
-                                .padding(.trailing)
-                        }
-                        
-                    }
-                }
+        init() {
+            if let data = try? Data(contentsOf: savePath),
+               let decodedLocations = try? JSONDecoder().decode([Location].self, from: data) {
+                locations = decodedLocations
+            } else {
+                locations = []
             }
-            .sheet(item: $viewModel.selectedPlace) { place in
-                EditView(location: place) { newLocation in
-                    viewModel.update(location: newLocation)
-                }
-            }.alert(viewModel.errorTitle,isPresented: $viewModel.hasError) {
-                Button("OK"){}
-            }
-            
-            
-        } else {
-            Button("Unlock Places") {
-                viewModel.authenticate()
-            }
-            .padding()
-            .background(.blue)
-            .foregroundColor(.white)
-            .clipShape(Capsule())
         }
+
+        
+        func addLocation() {
+            let newLocation = Location(id: UUID(), name: "New Location", description: " ", latitude: mapRegion.center.latitude, longitude: mapRegion.center.longitude)
+            locations.append(newLocation)
+            save()
+        }
+        
+        func update(location: Location) {
+            guard let selectedPlace = selectedPlace else { return }
+            
+            if let index = locations.firstIndex(of: selectedPlace) {
+                locations[index] = location
+            }
+            save()  }
+        
+        func save() {
+            do {
+                let data = try JSONEncoder().encode(locations)
+                try data.write(to: savePath, options: [.atomic, .completeFileProtection])
+            } catch {                print("Unable to save data.")
+            }
+        }
+        func authenticate() {
+            let context = LAContext()
+            var error: NSError?
+            
+            if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+                let reason = "Please authenticate yourself to unlock your places." //Reason for TouchId here. Reason for FaceId in Info.plist
+                
+                context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                    if success {
+                        Task {
+                            await MainActor.run{
+                                self.isUnlocked = true
+                            }
+                        }
+                    } else {
+                        Task {
+                            await MainActor.run{
+                                self.errorTitle = "Authentication failed"
+                                self.errorMessage = "Authentication with Face ID or Touch ID failed. Please try again."
+                                self.hasError = true
+                            }
+                        }
+                    }
+                }
+            } else {
+                errorTitle = "Authentication failed"
+                errorMessage = "Biometric authentication is not supported on your device."
+            }
+            print("Has error 6: \(self.hasError)")
+        }
+
     }
-    
-
 }
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
-
-
-
-
 
 
 
